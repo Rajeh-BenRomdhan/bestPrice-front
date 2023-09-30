f15fcb8385847b92b7ddb56a628ab8b57319c398
NizarAffess <nizaraffess@gmail.com>
2023-09-30 17:34:04 +0100

    Set up the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import coverImage from "../assets/cover.png"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="alert alert-info" role="alert">
+      <img src="/images/profile.png" width="150px" alt="Avatar" />
+      <h1>Jane Doe</h1>
+      <p>Hello my name is Jane Doe, I'am a Fullstack Web Developper</p>
+      <img src={coverImage} style={{ width: "100%" }} />
+    </div>
   );
 }
 
