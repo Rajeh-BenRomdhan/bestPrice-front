39c40bc4bf5cd0d0ef1ee801363acd3f0cdffe65
NizarAffess <nizaraffess@gmail.com>
2023-09-30 15:47:30 +0100

    Import default white Navbar from bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar.jsx"
+
 function App() {
 
   return (
     <div>
+      <Navbar />
       <h1 className="heading">Hello React</h1>
       <button className="btn btn-primary">Click</button>
     </div>
