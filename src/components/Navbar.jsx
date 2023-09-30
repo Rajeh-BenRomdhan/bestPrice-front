c01332bb583b24f190536f8a2aec81a1fc846477
NizarAffess <nizaraffess@gmail.com>
2023-09-30 16:58:34 +0100

    Add nav links in the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -9,16 +11,16 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <NavLink className="nav-link" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Skills</a>
+              <NavLink className="nav-link" to="/skills">Skills</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
+              <NavLink className="nav-link" to="/projects">Projects</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#" >Contact</a>
+              <NavLink className="nav-link" to="/contact">Contact</NavLink>
             </li>
           </ul>
         </div>
