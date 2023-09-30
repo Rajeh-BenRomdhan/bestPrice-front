36f482c2831082c2d35f2e6f73054d7f21fe0dc0
NizarAffess <nizaraffess@gmail.com>
2023-09-30 16:49:38 +0100

    Set up react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar.jsx"
+import Home from "./pages/Home.jsx"
+import Skills from "./pages/Skills.jsx"
+import Projects from "./pages/Projects.jsx"
+import Contact from "./pages/Contact.jsx"
 
 function App() {
 
   return (
     <div>
-      <Navbar />
-      <h1 className="heading">Hello React</h1>
-      <button className="btn btn-primary">Click</button>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
