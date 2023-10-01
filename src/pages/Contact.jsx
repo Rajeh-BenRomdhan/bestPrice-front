4a9de315021aa42a9b9670edc569576b22621e9a
NizarAffess <nizaraffess@gmail.com>
2023-10-01 17:21:39 +0100

    Set up the Contact page using "bootstrap-icons"

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,24 @@
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="container mt-4">
+      <h2>Contact Us</h2>
+      <div>
+        <i className="bi bi-envelope-fill"></i>
+        <span className="ms-2">
+          mike@gmail.com
+        </span>
+      </div>
+      <div className="mt-3">
+        <i className="bi bi-phone-fill"></i>
+        <span>123546789</span>
+      </div>
+      <div className="mt-3">
+        <i className="bi bi-geo-alt-fill"></i>
+        <span>
+          Route Tunis, Sakiet Ezzit, 3050, Tunisie
+        </span>
+      </div>
+    </div>
   )
 }
 
